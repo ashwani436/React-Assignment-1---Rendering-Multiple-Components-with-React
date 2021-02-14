c0543abf7d9416aa7d7ecedf8b602f1887ffbeda
ashwani436 <sharmagpl786@gmail.com>
2021-02-14 14:29:43 +0530

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p data-ns-test="project-name" >Hacker News Clone Using React</p>
+      <p data-ns-test="project-description">we're making the public Hacker News data available in near real time. 
+      Firebase enables easy access from Android, iOS and the web. Servers aren't left out</p>
+    </div>
   )
 }
 
